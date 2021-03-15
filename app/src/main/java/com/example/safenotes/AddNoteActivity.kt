c9df3063a981898703a7f74b1feefc4af2e4946e
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-03-15 18:28:20 +0100

    Add menu

--- app/src/main/java/com/example/safenotes/AddNoteActivity.kt
+++ app/src/main/java/com/example/safenotes/AddNoteActivity.kt
@@ -2,10 +2,28 @@ package com.example.safenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+    }
+
+
+    // Menu functionality
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_note, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.save_menu_item) {
+            TODO("Save functionality")
+        } else if (item.itemId == R.id.back_menu_item) {
+            TODO("Back functionality")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/safenotes/HomeScreenActivity.kt
+++ app/src/main/java/com/example/safenotes/HomeScreenActivity.kt
@@ -2,11 +2,28 @@ package com.example.safenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
         setSupportActionBar(findViewById(R.id.appbar))
+    }
+
+
+    // Menu functionality
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home_screen, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout_menu_item) {
+            //logout()
+            TODO("Logout functionality")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
